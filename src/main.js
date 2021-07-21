c8b380ffbceb0c93f74d0912ade3f6ac475e9129
EmilRegis <a730ut@gmail.com>
2021-07-21 18:24:00 +0600

    rebuild after fixies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'material-design-icons-iconfont';
-import VueSocialSharing from 'vue-social-sharing'
 
-createApp(App).use(store).use(router).use(VueSocialSharing).mount('#app');
+createApp(App).use(store).use(router).mount('#app');
